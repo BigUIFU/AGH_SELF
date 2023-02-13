12c0f8aad31721618b66a293fb15fe3f145fa3f0
BigUIFU <piuifu@outlook.com>
2023-02-14 00:19:24 +0800

    Update upsteam.sh

--- DNS/upsteam.sh
+++ DNS/upsteam.sh
@@ -2,7 +2,7 @@
 set -e
 DATE=`date --rfc-3339 sec`
 echo "$DATE: Getting data updates..."
-curl -o "/var/tmp/default.upstream" https://raw.githubusercontent.com/BigUIFU/2022/main/DNS/default-dns.conf
+curl -o "/var/tmp/default.upstream" "https://raw.githubusercontent.com/BigUIFU/AGH_SELF/main/DNS/default-dns.conf"
 curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/var/tmp/chinalist.upstream"
 echo "$DATE: Processing data format..."
 cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
